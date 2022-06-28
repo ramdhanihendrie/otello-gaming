25e5d35c4cefd84eb096149ce1fddd5f2653b878
ramdhanihendrie <50083363+ramdhanihendrie@users.noreply.github.com>
2022-06-28 18:53:52 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// const games = ["Ragnarok", "Red Dead Redemtion 2", "Among Us", "Call of Duty", "Fortnite", "Genshin Impact", "GTA V", "Mobile Legends", "PUBG Mobile"]
+
+// games.map((game) => {
+//   const container = document.getElementsByClassName("card-container")  
+//   container.write(
+//     `
+//       <div class="card col-md-4">
+//         <img idsrc="" class="card-img-top" alt="games-image">
+//         <div class="card-body d-flex flex-column">
+//           <h5 class="card-title text-white">testasdasd</h5>
+//           <a href="#" class="card-detail fw-bold my-2">See Details</a>
+//           <a href="#" class="btn btn-red">Claim</a>
+//         </div>
+//       </div>
+//     `
+//   )
+// })
