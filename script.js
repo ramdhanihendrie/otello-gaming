0a9e7308fb72bd531f5c3d79422e4104e7e261d9
ramdhanihendrie <50083363+ramdhanihendrie@users.noreply.github.com>
2022-07-03 13:19:38 +0700

    create internet package page

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-const gameDetail = document.getElementsByClassName('card-detail')
+// Script specific for index page
+function indexScript() {
+  const gameDetail = document.getElementsByClassName('card-detail')
 
+  for(let i = 0; i < gameDetail.length; i++){
+    gameDetail[i].addEventListener('click', event => {
+      const game = event.target.dataset.bsGame
 
-for(let i = 0; i < gameDetail.length; i++){
-  gameDetail[i].addEventListener('click', event => {
-    const game = event.target.dataset.bsGame
+      const gameTitle = document.querySelector('#games-title')
+      gameTitle.textContent = `${game}`
 
-    const gameTitle = document.querySelector('#games-title')
-    gameTitle.textContent = `${game}`
+      const detailImage = document.querySelector('#detail-image')
+      detailImage.setAttribute('src', `./assets/games/${game}.png`)
+    })
+  }
 
-    const detailImage = document.querySelector('#detail-image')
-    detailImage.setAttribute('src', `./assets/games/${game}.png`)
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+  const btnCopy = document.querySelector('#btn-copy-code')
+  btnCopy.addEventListener('click', () => {
+    const copyText = document.getElementById("redeem-code").innerText;
+    
+    navigator.clipboard.writeText(copyText);
   })
 }
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-
-const btnCopy = document.querySelector('#btn-copy-code')
-btnCopy.addEventListener('click', () => {
-  const copyText = document.getElementById("redeem-code").innerText;
-  
-  navigator.clipboard.writeText(copyText);
-})
-
